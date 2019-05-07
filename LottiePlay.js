cf6c458e24a57dda6bf9d7279bab50996bd34d1e
Minky <minky.lee430@gmail.com>
2019-05-07 22:30:41 +0100

    돼라3

--- LottiePlay.js
+++ LottiePlay.js
@@ -3,7 +3,7 @@
 var animation = bodymovin.loadAnimation({
   container: document.getElementById('lottie'), // Required
   path: 'hello.json', // Required
-  renderer: 'svg/canvas/html', // Required
+  renderer: 'svg', // Required
   loop: true, // Optional
   autoplay: true, // Optional
 })
